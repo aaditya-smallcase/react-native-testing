99f2abc7782eb779897f241349ffc68ff3c275f9
aaditya singh <aaditya.singh@smallcase.com>
2024-09-19 13:52:26 +0530

    fix: metro change to work

--- example/metro.config.js
+++ example/metro.config.js
@@ -5,14 +5,19 @@ const pkg = require('../package.json');
 
 const root = path.resolve(__dirname, '..');
 
-/**
- * Metro configuration
- * https://facebook.github.io/metro/docs/configuration
- *
- * @type {import('metro-config').MetroConfig}
- */
-module.exports = getConfig(getDefaultConfig(__dirname), {
-  root,
-  pkg,
-  project: __dirname,
-});
+// Metro configuration
+module.exports = {
+  ...getConfig(getDefaultConfig(__dirname), {
+    root,
+    pkg,
+    project: __dirname,
+  }),
+  resolver: {
+    extraNodeModules: {
+      'react-native': path.resolve(__dirname, 'node_modules/react-native'),
+    },
+  },
+  watchFolders: [
+    path.resolve(__dirname, '..'), // Watch the parent directory where the library is located
+  ],
+};
